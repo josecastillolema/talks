a0d6946ef31daf56f2edfb7bc68d161d9d4cf745
Jose Castillo Lema <josecastillolema@gmail.com>
2025-05-22 20:37:41 -0400

    Thu May 22 08:37:41 PM EDT 2025
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- 2025-fp-madrid/example.ml
+++ 2025-fp-madrid/example.ml
@@ -2,8 +2,10 @@ type contacto =
   | Email of string
   | Direccion of string
   | EmailyDireccion of string * string
+  | NumeroTlfno of int
 
 let print = function
   | Email e -> e
   | Direccion d -> d
   | EmailyDireccion (e,d) -> d
+  | NumeroTlfno t -> string_of_int t
